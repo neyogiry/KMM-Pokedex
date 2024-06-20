ba3a51f76feb8a5029636a82c431756aac1bf26e
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-06-19 22:15:19 -0500

    Add Koin initializater

--- Pokedex/shared/src/commonMain/kotlin/di/Koin.kt
+++ Pokedex/shared/src/commonMain/kotlin/di/Koin.kt
@@ -1,5 +1,12 @@
 package di
 
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 val sharedModule = module {  }
+
+fun initKoin() {
+    startKoin {
+        modules(sharedModule)
+    }
+}
