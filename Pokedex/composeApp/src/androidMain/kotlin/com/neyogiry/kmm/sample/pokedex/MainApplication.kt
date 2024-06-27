97340576aead3d554ea8296354de437bbc2d0595
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-06-26 19:30:29 -0500

    Add MainApplication to android app

---
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.neyogiry.kmm.sample.pokedex
+
+import android.app.Application
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
