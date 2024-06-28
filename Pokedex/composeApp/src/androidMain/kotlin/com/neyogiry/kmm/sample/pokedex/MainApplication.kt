ac170f5b99668f2c103135715ae2acd234f4d9ca
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-06-27 21:15:16 -0500

    init Koin from android app

--- Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/MainApplication.kt
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/MainApplication.kt
@@ -1,9 +1,11 @@
 package com.neyogiry.kmm.sample.pokedex
 
 import android.app.Application
+import di.initKoin
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initKoin()
     }
 }
