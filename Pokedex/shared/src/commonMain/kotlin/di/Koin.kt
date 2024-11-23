cad940974eb8d5447267a176b6a5f59d6e222043
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-11-23 08:23:32 -0500

    Add HttpClient dependency from koin

--- Pokedex/shared/src/commonMain/kotlin/di/Koin.kt
+++ Pokedex/shared/src/commonMain/kotlin/di/Koin.kt
@@ -1,12 +1,26 @@
 package di
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
-val sharedModule = module {  }
+val sharedModule = module {
+    single< HttpClient> { httpClient() }
+}
 
 fun initKoin() {
     startKoin {
         modules(sharedModule)
+    }
+}
+
+private fun httpClient() = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+        })
     }
 }
