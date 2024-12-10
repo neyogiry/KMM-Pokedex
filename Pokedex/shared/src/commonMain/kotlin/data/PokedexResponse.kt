77a29619f9b9ba6aa6e412748b94d6c7aeb7f3a1
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-12-10 09:50:05 -0500

    Add Serializable to data model

--- Pokedex/shared/src/commonMain/kotlin/data/PokedexResponse.kt
+++ Pokedex/shared/src/commonMain/kotlin/data/PokedexResponse.kt
@@ -1,11 +1,15 @@
 package data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PokedexResponse(
     val count: Int?,
     val next: String?,
     val results: List<PokemonResponse>?,
 )
 
+@Serializable
 data class PokemonResponse(
     val name: String?,
     val url: String?,
