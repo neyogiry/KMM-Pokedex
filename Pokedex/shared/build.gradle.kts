2bec44ad9e8e6e9f3e2e298d24ddfd343c2a0e43
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-12-12 22:32:54 -0500

    Add androidx-room-runtime dependency

--- Pokedex/shared/build.gradle.kts
+++ Pokedex/shared/build.gradle.kts
@@ -35,6 +35,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.androidx.room.runtime)
         }
         androidMain.dependencies {
             implementation(libs.ktor.client.android)
