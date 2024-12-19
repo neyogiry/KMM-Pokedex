bc56655e12b77b580425acbd99227555235a3449
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-12-19 08:58:51 -0500

    Add Pokemon entity to data layer

--- Pokedex/shared/src/commonMain/kotlin/data/database/PokedexDatabase.kt
+++ Pokedex/shared/src/commonMain/kotlin/data/database/PokedexDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
+import data.model.Pokemon
 
-@Database(entities = [], version = 1)
+@Database(entities = [Pokemon::class], version = 1)
 @ConstructedBy(AppDatabaseConstructor::class)
 abstract class PokedexDatabase : RoomDatabase()
 
---
+++ Pokedex/shared/src/commonMain/kotlin/data/model/Pokemon.kt
@@ -0,0 +1,11 @@
+package data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Pokemon(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val url: String,
+)
