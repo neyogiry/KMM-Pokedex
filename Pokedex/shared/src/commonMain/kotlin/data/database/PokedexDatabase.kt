1b45b0edcef4e169af62ebd86bca1c8d0a49faec
neyogiry <8197869+neyogiry@users.noreply.github.com>
2024-12-18 21:10:47 -0500

    Define RoomDatabaseConstructor

--- Pokedex/shared/src/commonMain/kotlin/data/database/PokedexDatabase.kt
+++ Pokedex/shared/src/commonMain/kotlin/data/database/PokedexDatabase.kt
@@ -1,7 +1,15 @@
 package data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 
 @Database(entities = [], version = 1)
+@ConstructedBy(AppDatabaseConstructor::class)
 abstract class PokedexDatabase : RoomDatabase()
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<PokedexDatabase> {
+    override fun initialize(): PokedexDatabase
+}
