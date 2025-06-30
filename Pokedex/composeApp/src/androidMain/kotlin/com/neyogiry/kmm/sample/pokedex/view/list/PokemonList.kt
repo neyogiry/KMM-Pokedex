7907a4c6d4b6560bd08c076bcfc40ddf6f6b5f0c
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-06-30 09:01:01 -0500

    refactor: Migrate to Material3

--- Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/view/list/PokemonList.kt
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/view/list/PokemonList.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
