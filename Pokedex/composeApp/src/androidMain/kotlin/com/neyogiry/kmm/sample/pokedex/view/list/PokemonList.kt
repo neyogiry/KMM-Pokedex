898335048828c6fe4d90f35f53d906e577d16d4b
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-07-02 10:52:16 -0500

    Add TopAppBar

--- Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/view/list/PokemonList.kt
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/view/list/PokemonList.kt
@@ -8,17 +8,24 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -31,18 +38,47 @@ fun PokemonListScreen(
     viewModel: PokemonListViewModel = koinViewModel<PokemonListViewModel>()
 ) {
     val viewState by viewModel.state.collectAsState()
-    PokemonListContent(
-        list = viewState.pokemonList
+
+    Scaffold(
+        topBar = { PokedexAppBar() }
+    ) { paddingValues ->
+        PokemonList(
+            list = viewState.pokemonList,
+            paddingValues = paddingValues
+        )
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PokedexAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Pokedex",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                textAlign = TextAlign.Center
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        ),
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     )
 }
 
 @Composable
-fun PokemonListContent(
-    list: List<Pokemon> = emptyList()
+fun PokemonList(
+    list: List<Pokemon>,
+    paddingValues: PaddingValues
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(10.dp)
+        contentPadding = paddingValues
     ) {
         items(list) { pokemon ->
             PokemonItem(pokemon)
