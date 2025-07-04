d8465dcca835c20f30c9988c70c82181fada29a8
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-07-04 08:53:13 -0500

    fix: ViewModel dependency

--- Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/di/AppModule.kt
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.neyogiry.kmm.sample.pokedex.di
 
+import com.neyogiry.kmm.sample.pokedex.view.list.PokemonListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
+    viewModelOf(::PokemonListViewModel)
 }
