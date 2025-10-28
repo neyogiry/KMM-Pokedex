f798d1f7da5dcb12e7b8d15ae1e3fb4121ad8572
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-10-28 09:58:49 -0500

    Bump koin from 3.5.3 to 4.1.1

--- Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/di/AppModule.kt
+++ Pokedex/composeApp/src/androidMain/kotlin/com/neyogiry/kmm/sample/pokedex/di/AppModule.kt
@@ -1,7 +1,7 @@
 package com.neyogiry.kmm.sample.pokedex.di
 
 import com.neyogiry.kmm.sample.pokedex.view.list.PokemonListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
